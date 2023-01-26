e695d984c9cebdcbb723f44c8906120b5a13093f
Denys Mazanov <dmazanov@mail.ru>
2023-01-26 21:41:34 +0100

    configuration Prettier

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          alt="roman"
         >
           Learn React
         </a>
