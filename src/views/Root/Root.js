50497925bb7fc335846f444986e74c59686eabb3
Denys Mazanov <dmazanov@mail.ru>
2023-01-28 15:07:24 +0100

    created project structure. added absolute imports. created button component

---
+++ src/views/Root/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+function Root() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <Button> Test Button</Button>
+    </div>
+  );
+}
+
+export default Root;
