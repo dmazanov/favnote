a797607768b1c72449879d22ae23822d6490fb49
Denys Mazanov <dmazanov@mail.ru>
2023-01-28 14:14:09 +0100

    added styled-components and configure it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const MyButton = styled.button`
+  border: 2px solid blue;
+  padding: 30px;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MyButton>This is my button</MyButton>
         <a
           className="App-link"
           href="https://reactjs.org"
